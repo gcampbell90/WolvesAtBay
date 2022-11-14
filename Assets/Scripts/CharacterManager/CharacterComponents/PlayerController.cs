15c6f9bf585675691c47bb08baa34000def55953
Gary Campbell <gary.campbell3690@gmail.com>
2022-11-14 10:05:04 +0000

    Added shield and defense move

--- Assets/Scripts/CharacterManager/CharacterComponents/PlayerController.cs
+++ Assets/Scripts/CharacterManager/CharacterComponents/PlayerController.cs
@@ -14,11 +14,15 @@ public class PlayerController : MonoBehaviour
     //private Rigidbody rb;
 
     //float screenHalfWidthInWorldUnits
-    public Transform Pivot { get; set; }
+    public Transform SwordPivot { get; set; }
+    public Transform ShieldPivot { get; set; }
     private void Awake()
     {
-        Pivot = gameObject.GetComponentInChildren<Transform>().GetChild(0).transform;
-        Pivot.rotation = Quaternion.Euler(0, -70, 0);
+        SwordPivot = gameObject.GetComponentInChildren<Transform>().GetChild(0).transform;
+        SwordPivot.rotation = Quaternion.Euler(0, -70, 0);
+
+        ShieldPivot = gameObject.GetComponentInChildren<Transform>().GetChild(1).transform;
+
     }
     private void Start()
     {
@@ -39,13 +43,18 @@ public class PlayerController : MonoBehaviour
         // Using some math to calculate the point of intersection between the line going through the camera and the mouse position with the XZ-Plane
         float t = cam.transform.position.y / (cam.transform.position.y - point.y);
         Vector3 finalPoint = new Vector3(t * (point.x - cam.transform.position.x) + cam.transform.position.x, 1, t * (point.z - cam.transform.position.z) + cam.transform.position.z);
-        
+
         //Rotating the object to that point
         transform.LookAt(finalPoint, Vector3.up);
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             StartCoroutine(AttackMove());
+            //print("space key was pressed");
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            StartCoroutine(DefensiveMove());
             //print("space key was pressed");
         }
 
@@ -67,7 +76,7 @@ public class PlayerController : MonoBehaviour
         while (progress <= 1f)
         {
             // Do some nice animation
-            Pivot.localRotation = Quaternion.Slerp(Quaternion.Euler(0, -70, 0), Quaternion.Euler(0, rot, 0), progress);
+            SwordPivot.localRotation = Quaternion.Slerp(Quaternion.Euler(0, -70, 0), Quaternion.Euler(0, rot, 0), progress);
             progress += Time.deltaTime / duration;
 
             // Make the coroutine wait for a moment
@@ -75,18 +84,38 @@ public class PlayerController : MonoBehaviour
         }
         EffectController.Instance.DisableSwordTrail();
 
-        Pivot.localRotation = Quaternion.Euler(0, -70, 0);
+        SwordPivot.localRotation = Quaternion.Euler(0, -70, 0);
     }
+    //TODO: Turn into animation call/method...abstract away from player controller.
+    private IEnumerator DefensiveMove()
+    {
+        //float rot = 40;
+        float timer = 0f;
+        float duration = 2f;
 
-    //void OnTriggerEnter(Collider triggerCollider)
-    //{
-    //    if (triggerCollider.tag == "Enemy") 
-    //    {
-    //        if (OnCubeDeath != null)
-    //        {
-    //            OnCubeDeath();
-    //        }
-    //        Destroy(gameObject);
-    //    }
-    //}
+        while (!Input.GetKeyUp(KeyCode.Mouse1) && timer <= 1f)
+        {
+            SwordPivot.gameObject.SetActive(false);
+            ShieldPivot.gameObject.SetActive(true);
+
+            yield return null;
+
+            timer += Time.deltaTime;
+
+        }
+        ShieldPivot.gameObject.SetActive(false);
+        SwordPivot.gameObject.SetActive(true);
+    }
 }
+
+//void OnTriggerEnter(Collider triggerCollider)
+//{
+//    if (triggerCollider.tag == "Enemy") 
+//    {
+//        if (OnCubeDeath != null)
+//        {
+//            OnCubeDeath();
+//        }
+//        Destroy(gameObject);
+//    }
+//}
